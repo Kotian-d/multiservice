0dc4e8ac4511f75249711e2777712ed2631a5139
Shreyas-2004-ai <shreyasssanil62@gmail.com>
2026-02-12 14:11:29 +0530

    Third commit

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(express.json());
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 
 const connectDb = async () => {
   try {
